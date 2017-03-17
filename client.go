6e37832cd73b29ed2c40be285216b4c5607bd1b9
Brian Brazil <brian.brazil@robustperception.io>
2017-03-17 13:49:09 +0000

    Make client more resilient and performant

--- client.go
+++ client.go
@@ -6,26 +6,16 @@ import (
 	"log"
 	"net/http"
 	"strings"
+	"time"
 )
 
-func loop() {
+func doScrape(request *http.Request) {
 	client := &http.Client{}
-	fqdn := "localhost"
-
-	resp, err := client.Post("http://localhost:1234/poll", "", strings.NewReader(fqdn))
-	if err != nil {
-		log.Fatal(err)
-		return
-	}
-	defer resp.Body.Close()
-	request, _ := http.ReadRequest(bufio.NewReader(resp.Body))
 	id := request.Header.Get("id") // Needed so they can be linked.
-	log.Printf("Got request for %s", request.URL.String())
-	request.RequestURI = ""
 
 	scrapeResp, err := client.Do(request)
 	if err != nil {
-		log.Fatal(err)
+		log.Printf("Failed to scrape %s: %s", request.URL.String(), err)
 		return
 	}
 	log.Printf("Scraped %s", request.URL.String())
@@ -36,10 +26,28 @@ func loop() {
 
 	_, err = client.Post("http://localhost:1234/push", "", buf)
 	if err != nil {
-		log.Fatal(err)
+		log.Printf("Failed to push scrape result for %s: %s", request.URL.String(), err)
 		return
 	}
 	log.Printf("Pushed scrape result for %s", request.URL.String())
+}
+
+func loop() {
+	client := &http.Client{}
+	fqdn := "localhost"
+
+	resp, err := client.Post("http://localhost:1234/poll", "", strings.NewReader(fqdn))
+	if err != nil {
+		log.Printf("Error polling: %s", err)
+		time.Sleep(time.Second) // Don't pound the server.
+		return
+	}
+	defer resp.Body.Close()
+	request, _ := http.ReadRequest(bufio.NewReader(resp.Body))
+	log.Printf("Got request for %s", request.URL.String())
+	request.RequestURI = ""
+
+	go doScrape(request)
 }
 
 func main() {
