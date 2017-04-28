3ebe6bdd8359ac2de6b08133d0d436be2d670add
Brian Brazil <brian.brazil@robustperception.io>
2017-04-28 13:36:58 +0100

    Split out coordinator to seperate file

--- proxy/proxy.go
+++ proxy/proxy.go
@@ -8,123 +8,12 @@ import (
 	"io"
 	"io/ioutil"
 	"net/http"
-	"os"
 	"strings"
-	"sync"
-	"sync/atomic"
-	"time"
 
 	"github.com/prometheus/common/log"
 
 	"gitlab.com/robust-perception/tug_of_war/util"
 )
-
-type Coordinator struct {
-	mu        sync.Mutex
-	waiting   map[string]chan *http.Request
-	responses map[string]chan *http.Response
-}
-
-func NewCoordinator() *Coordinator {
-	return &Coordinator{
-		waiting:   map[string]chan *http.Request{},
-		responses: map[string]chan *http.Response{},
-	}
-}
-
-var idCounter int64
-
-// Generate a unique ID
-func genId() string {
-	id := atomic.AddInt64(&idCounter, 1)
-	// TODO: Add MAC address.
-	// TODO: Sign these to prevent spoofing.
-	return fmt.Sprintf("%d-%d-%d", time.Now().Unix(), id, os.Getpid())
-}
-
-func (c *Coordinator) getRequestChannel(fqdn string) chan *http.Request {
-	c.mu.Lock()
-	defer c.mu.Unlock()
-	ch, ok := c.waiting[fqdn]
-	if !ok {
-		ch = make(chan *http.Request)
-		c.waiting[fqdn] = ch
-	}
-	return ch
-}
-
-func (c *Coordinator) getResponseChannel(id string) chan *http.Response {
-	c.mu.Lock()
-	defer c.mu.Unlock()
-	ch, ok := c.responses[id]
-	if !ok {
-		ch = make(chan *http.Response)
-		c.responses[id] = ch
-	}
-	return ch
-}
-
-// Remove a response channel. Idempotent.
-func (c *Coordinator) removeResponseChannel(id string) {
-	c.mu.Lock()
-	defer c.mu.Unlock()
-	delete(c.responses, id)
-}
-
-// Request a scrape.
-func (c *Coordinator) DoScrape(ctx context.Context, r *http.Request) (*http.Response, error) {
-	id := genId()
-	log.With("scrape_id", id).With("url", r.URL.String()).Info("DoScrape")
-	r.Header.Add("Id", id)
-	select {
-	case <-ctx.Done():
-		return nil, fmt.Errorf("Matching client not found for %q: %s", r.URL.String(), ctx.Err())
-	case c.getRequestChannel(r.URL.Hostname()) <- r:
-	}
-
-	respCh := c.getResponseChannel(id)
-	defer c.removeResponseChannel(id)
-
-	select {
-	case <-ctx.Done():
-		return nil, ctx.Err()
-	case resp := <-respCh:
-		return resp, nil
-	}
-}
-
-// Client registering to accept a scrape request. Blocking.
-func (c *Coordinator) WaitForScrapeInstruction(fqdn string) (*http.Request, error) {
-	log.With("fqdn", fqdn).Info("WaitForScrapeInstruction")
-	// TODO: What if the client times out?
-	ch := c.getRequestChannel(fqdn)
-	for {
-		request := <-ch
-		select {
-		case <-request.Context().Done():
-			// Request has timed out, get another one.
-		default:
-			return request, nil
-		}
-	}
-}
-
-// Client sending a scrape result in.
-func (c *Coordinator) ScrapeResult(r *http.Response) error {
-	id := r.Header.Get("Id")
-	log.With("scrape_id", id).Info("ScrapeResult")
-	ctx, _ := context.WithTimeout(context.Background(), util.GetScrapeTimeout(r.Header))
-	// Don't expose internal headers.
-	r.Header.Del("Id")
-	r.Header.Del("X-Prometheus-Scrape-Timeout-Seconds")
-	select {
-	case c.getResponseChannel(id) <- r:
-		return nil
-	case <-ctx.Done():
-		c.removeResponseChannel(id)
-		return ctx.Err()
-	}
-}
 
 func copyHttpResponse(resp *http.Response, w http.ResponseWriter) {
 	for k, v := range resp.Header {
