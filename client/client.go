9b48ab906018687cab69a817efb1f5697de40741
Brint E. Kriebel <github@bekit.net>
2019-03-12 11:50:32 -0700

    client: Use sane default values for the custom transport
    When setting up the custom transport for TLS support, the values for
    timeouts, keepalive, and connection counts are all defaulting to zero
    values. This can cause the client to not reconnect to a server that has
    become unavailable.
    
    Use the values of DefaultTransport in net/http to set up the custom
    transport with appropriate settings.

--- client/client.go
+++ client/client.go
@@ -9,6 +9,7 @@ import (
 	"errors"
 	"fmt"
 	"io/ioutil"
+	"net"
 	"net/http"
 	"net/url"
 	"os"
@@ -227,7 +228,19 @@ func main() {
 		}()
 	}
 
-	transport := &http.Transport{TLSClientConfig: tlsConfig}
+	transport := &http.Transport{
+		Proxy: http.ProxyFromEnvironment,
+		DialContext: (&net.Dialer{
+			Timeout:   30 * time.Second,
+			KeepAlive: 30 * time.Second,
+			DualStack: true,
+		}).DialContext,
+		MaxIdleConns:          100,
+		IdleConnTimeout:       90 * time.Second,
+		TLSHandshakeTimeout:   10 * time.Second,
+		ExpectContinueTimeout: 1 * time.Second,
+		TLSClientConfig: tlsConfig,
+	}
 
 	for {
 		err := loop(coordinator, transport)
