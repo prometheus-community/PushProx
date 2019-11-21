e10df6b9c06cac6a9b837e4f34b7d87de817a4e6
anton troyanov <anton@troyanov.net>
2019-11-21 14:17:41 +0100

    Fix: Strict mode to enforce FQDN
    Go docs:
    https://golang.org/pkg/net/url/#Parse
    > Trying to parse a hostname and path without a scheme is invalid but may not necessarily return an error, due to parsing ambiguities.
    
    ```
    fqdnURL, err := url.Parse(*myFqdn)
    if err != nil {
        c.handleErr(request, client, err)
        return
    }
    ```
    Example:
    myFqdn was set to `b607b847b180`, scheme is missing, but no error (as mentioned in the docs).
    
    We don't need to treat myFqdn as URL since it is not an URL but rather FQDN (which is schemaless)

--- client/client.go
+++ client/client.go
@@ -104,12 +104,7 @@ func (c *Coordinator) doScrape(request *http.Request, client *http.Client) {
 		request.URL.RawQuery = params.Encode()
 	}
 
-	fqdnURL, err := url.Parse(*myFqdn)
-	if err != nil {
-		c.handleErr(request, client, err)
-		return
-	}
-	if request.URL.Hostname() != fqdnURL.Hostname() {
+	if request.URL.Hostname() != *myFqdn {
 		c.handleErr(request, client, errors.New("scrape target doesn't match client fqdn"))
 		return
 	}
@@ -225,6 +220,12 @@ func main() {
 	kingpin.Parse()
 	logger := promlog.New(&promlogConfig)
 	coordinator := Coordinator{logger: logger}
+
+	if myFqdn == nil || strings.TrimSpace(*myFqdn) == "" {
+		level.Error(coordinator.logger).Log("msg", "client fqdn cannot be empty")
+		os.Exit(1)
+	}
+
 	if *proxyURL == "" {
 		level.Error(coordinator.logger).Log("msg", "--proxy-url flag must be specified.")
 		os.Exit(1)
